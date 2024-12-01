e467fea5b424015d6bcd9f22f34bb1a211c0e115
Marttify <aguilarmartin977@gmail.com>
2024-12-01 10:26:50 -0300

    Initial Commit

---
+++ CRUDApp/CRUDAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDAppApp.swift
+//  CRUDApp
+//
+//  Created by Martin Aguilar on 01/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CRUDAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRUDAppTests/CRUDAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDAppTests.swift
+//  CRUDAppTests
+//
+//  Created by Martin Aguilar on 01/12/2024.
+//
+
+import Testing
+@testable import CRUDApp
+
+struct CRUDAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
